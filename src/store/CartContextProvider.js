915587c263c22077f11f781874e085437dd1274a
Aditya Singh <adityasingh220598@gmail.com>
2022-04-24 17:42:55 +0530

    Adding the Cart reducer

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -1,13 +1,45 @@
 import CartContext from "./cart-context";
+import {useReducer} from "react";
 
+
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_ITEM":
+            return {
+                items: [...state.items, action.item],
+                totalAmount: state.totalAmount + action.item.price
+
+            }
+        case "REMOVE_ITEM":
+            return {
+                items: state.items.filter((item) => item.id !== action.id),
+                totalAmount: state.totalAmount - action.item.price
+
+            }
+        default:
+            return state
+    }
+}
 const CartContextProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, {
+        items: [],
+        totalAmount: 0
+    })
     const addItemToCartHandler = (item) => {
+        dispatchCartAction({
+            type: "ADD_ITEM",
+            item: item
+        })
     }
     const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({
+            type: "REMOVE_ITEM",
+            id: id
+        })
     }
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
     }
