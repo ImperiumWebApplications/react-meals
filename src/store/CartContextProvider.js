a8016bbabc9a976153260e1e7b309c8b1b8e51d8
Aditya Singh <adityasingh220598@gmail.com>
2022-04-24 21:36:57 +0530

    Reducer revamped

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -3,46 +3,42 @@ import {useReducer} from "react";
 
 
 const cartReducer = (state, action) => {
-    switch (action.type) {
-        case "ADD_ITEM":
-            let existingItemIndex = state.items.findIndex((element) => element.id === action.item.id)
-            let updatedItems;
-            if (existingItemIndex > -1) {
-                updatedItems = [...state.items]
-                updatedItems[existingItemIndex] = {
-                    ...updatedItems[existingItemIndex],
-                    amount: updatedItems[existingItemIndex].amount + 1,
-                }
-            } else {
-                updatedItems = [...state.items, action.item]
+    if (action.type === "ADD_ITEM") {
+        const existingCartItemIndex = state.items.findIndex((element) => element.id === action.item.id)
+        let updatedItems;
+        if (existingCartItemIndex > -1) {
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = {
+                ...state.items[existingCartItemIndex],
+                amount: state.items[existingCartItemIndex].amount + 1
             }
-
-            return {
-                items: updatedItems,
-                totalAmount: state.totalAmount + action.item.price
-
+        } else {
+            updatedItems = [...state.items, action.item]
+        }
+        return {
+            items: updatedItems,
+            totalAmount: state.totalAmount + action.item.price
+        }
+    } else if (action.type === "REMOVE_ITEM") {
+        const existingCartItemIndex = state.items.findIndex((element) => element.id === action.id)
+        let updatedItems;
+        if (state.items[existingCartItemIndex].amount > 1) {
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = {
+                ...state.items[existingCartItemIndex],
+                amount: state.items[existingCartItemIndex].amount - 1
             }
-        case "REMOVE_ITEM":
-            let existingItemIndex1 = state.items.findIndex((element) => element.id === action.id)
-            let updatedItems1;
-            if (state.items[existingItemIndex1].amount > 1) {
-                updatedItems1 = [...state.items]
-                updatedItems1[existingItemIndex1] = {
-                    ...updatedItems1[existingItemIndex1],
-                    amount: updatedItems1[existingItemIndex1].amount - 1,
-                }
-
-            } else {
-                updatedItems1 = state.items.filter((element) => element.id !== action.id)
-            }
-            return {
-                items: updatedItems1,
-                totalAmount: state.totalAmount - state.items.find((element) => element.id === action.id).price
-
-            }
-        default:
-            return state
+        }
+        else{
+            updatedItems = state.items.filter((element)=> element.id !== action.id)
+        }
+        return {
+            items: updatedItems,
+            totalAmount: state.totalAmount - state.items.find((element) => element.id === action.id).price
+        }
     }
+
+    return state;
 }
 const CartContextProvider = (props) => {
     const [cartState, dispatchCartAction] = useReducer(cartReducer, {
